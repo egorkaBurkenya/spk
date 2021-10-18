0a083eea6258ffbcbd49fb87b1e068b27f7dbd6a
Desp1cable <Sagaidullaev_pr1.19@st.ithub.ru>
2021-10-18 15:41:53 +0300

    фоточки языков и ссылки

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import style from './MainPage.module.css'
+import Card from './Card.jsx'
+import javascriptImg from './assets/javascript.png'
+import pythonImg from './assets/python.png'
+import swiftImg from './assets/swift.png'
 
 const MainPage = () => {
     return (
-        <div>
-            hello MainPage
+        <div className={style.mainPage}>
+            <header className={style.quals}>
+                <h2 className={style.sectionTitle}>Квалификации</h2>
+                <div className={style.qualCards}>
+                    <Card cardPath={"/javascript"} cardImg={javascriptImg} cardText="Javascipt 2021 qualification"/>
+                    <Card cardImg={pythonImg} cardText="Python 2021 qualification"/>
+                    <Card cardImg={swiftImg} cardText="Swift 2021 qualification"/>
+                </div>
+            </header>
+            <section className={style.ceq}></section>
+            <section className={style.docs}></section>
+            <section className={style.news}></section>
+            <footer className={style.contacts}></footer>
         </div>
-    );
+);
 };
 
 export default MainPage;
