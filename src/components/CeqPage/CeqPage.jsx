8f6417b3ba5f746202f1ebabb0cdc6fa98b3cc43
egorkaBurkenya <sleepyegorka@gmail.com>
2021-10-21 16:57:08 +0300

    пул

--- src/components/CeqPage/CeqPage.jsx
+++ src/components/CeqPage/CeqPage.jsx
@@ -4,33 +4,6 @@ import CeqMainPage from "./CeqMainPage"
 import JavaScriptTest from "./JavaScriptTest"
 
 const CeqPage = () => {
-
-    const [test, setTest] = useState(
-        { "javaSctipr" : [
-        {   
-            uid: 0,
-            qestion: "Ты гей",
-            answers: ["Да", "Может быть", "Не уверен", "Точно да"],
-            rightAnswers: {
-                "Дa": false,
-                "Может быть": false,
-                "Не уверен": false,
-                "Точно да": true
-            }
-        },
-        {   
-            uid: 1,
-            qestion: "Что такое гей?",
-            answers: ["Стиль", "Я", "мой отец", "смысл жить"],
-            rightAnswers: {
-                "Стиль": false,
-                "Я": false,
-                "мой отец": false,
-                "смысл жить": true
-            }
-        }
-    ]})
-
     return (
         <Switch>
             <Route path={"/ceq/javascript"} component={JavaScriptTest} />
--- src/components/CeqPage/JavaScriptTest.jsx
+++ src/components/CeqPage/JavaScriptTest.jsx
@@ -1,104 +1,90 @@
 import React, {useState} from 'react';
-import style from './CeqPage.module.css';
+import {Link} from 'react-router-dom';
+import style from "./CeqPage.module.css";
 
 const JavaScriptTest = () => {
 
-    const [qustionUid, _setQustionUid] = useState(0);
-    const setQustionUid = (uid) => () => {
-        _setQustionUid(uid)
-        console.log(userAnswers);
-    }
-
-    const [userAnswers, setUserAnswers] = useState({})
-    const setUserAnswer = (uid, answer) => (e) => {
-        setUserAnswers({...userAnswers, [e.target.name]: {
-            answer: answer,
-            uid: uid
-        }})
-    }
-
     const test = [
-        {   
-            uid: 0,
-            qestion: "Какая типизация в JavaScript? ",
-            answers: ["Строгая", "Не строгая", "Гибкая", "Вариативная"],
-            rightAnswers: "Строгая"
+        {
+            number: 1,
+            qusion: 'Какую типизацию имеет JavaScript?', 
+            answers: [
+                "Строгую",
+                "Не строгую",
+                "Гибкую", 
+                "Обратную"
+            ],
+            rightAnswer: "Строгую"
         },
-        {   
-            uid: 1,
-            qestion: "Что такое гей?",
-            answers: ["Стиль", "Я", "мой отец", "смысл жить"],
-            rightAnswers: {
-                "Стиль": false,
-                "Я": false,
-                "мой отец": false,
-                "смысл жить": true
-            }
+        {
+            number: 2,
+            qusion: 'Что такое карирование?', 
+            answers: [
+                "Функция, которая вызывает функцию",
+                "Функция, которая вызывает саму себя",
+                "Функция, которая возвращает функцию", 
+                "Класс, который вызывает контруктор внутри себя"
+            ],
+            rightAnswer: "Функция, которая возвращает функцию"
+        },
+        {
+            number: 3,
+            qusion: 'В каком году появился JavaScript?', 
+            answers: [
+                "в сентябре 1995 года",
+                "в октябре 1995 года",
+                "в июне 1995 года", 
+                "в октябре 2001 года"
+            ],
+            rightAnswer: "в сентябре 1995 года"
         }
     ]
 
+    const [userAnswers, setUserAnswers] = useState({});
+
+    const addAnswer  = (qusionNumber) => {
+        return e => {
+            if (e.target.checked) {
+                setUserAnswers({...userAnswers, [qusionNumber]: e.target.id})
+            } else {
+                setUserAnswers({...userAnswers, [qusionNumber]: ""})
+            }
+        }
+    }
+
+    let level;
+    if (localStorage.level === undefined || localStorage.level === "") {
+        level = "Пройдите тестирование"
+    } else {
+        level = localStorage.level;
+    }
     return (
-        <div className={style.ceqPage}>
-            <h1 className={style.title}>
-                Тест на знания JavaScript
-            </h1>
-            <p className={style.description} style={{
-                width: "50%"
-            }}>
-            JavaScript — мультипарадигменный язык программирования. 
-            Поддерживает объектно-ориентированный, императивный и функциональный стили.
-            Является реализацией спецификации ECMAScript. 
-            JavaScript обычно используется как встраиваемый язык для программного доступа 
-            к объектам приложений. <br/><br/> 
-            <p style={{
-                color: "", 
-                margin: "5px",
-                borderBottom: "1px solid #999",  
-                paddingBottom: "5px",
-                fontSize: "20px"
-                }}>Пройдите тест для определения вашего уровня </p>
-            </p>
-            <div className={style.test}>
-                {test.map(qestion => {
-                    return (
-                        <div style={{
-                            display: qustionUid !== qestion.uid && 'none'
-                            }}
-                            className={style.qestion}>
-                            <h2 className={style.qestion__title}>
-                                {qestion.qestion}
-                            </h2>
-                            <div className={style.answers}>
-                                {qestion.answers.map(answer => {
-                                    const uid = Date.now().toString()
-                                    return (
-                                        <div className={style.answer}>
-                                        <input 
-                                        name={qestion.qestion}
-                                        type="checkbox" 
-                                        id={uid}
-                                        // value={false}
-                                        onChange={setUserAnswer(qestion.uid, answer)}
-                                          />
-                                        <label htmlFor={uid}>{answer}</label> 
-                                        </div>
-                                    )
-                                })} 
-                            </div>
-                            <div className={style.nav}>
-                                <button onClick={
-                                    setQustionUid(qestion.uid !== 0 ? qestion.uid - 1 : test.length - 1)}>
-                                        Назад
-                                    </button>
-                                <button onClick={
-                                    setQustionUid(qestion.uid !== test.length - 1 ? qestion.uid + 1 : 0)}>
-                                        Следующий вопрос
-                                </button>
-                            </div>
-                        </div>
-                    )
-                })}
-                
+        <div className={style.testPage}>
+            <div className={style.test__header}>
+                <Link to={"/ceq"} className={style.test__backBtn}>Назад</Link>
+                <h1> Оценка уровня владения JavaScript </h1>
+                <div className={style.test__userLevel}>
+                    <p>
+                        <span onClick={() => console.log(userAnswers)}>level:</span> {level}
+                    </p>
+                </div>
+            </div>
+            <div className={style.test__window}>
+                {test.map(qusion => 
+                <div className={style.test__qusion}>
+                    <h2>{qusion.number}. {qusion.qusion}</h2>
+                    {qusion.answers.map(answ => 
+                    <div className={style.test__answer}>
+                        <input 
+                        onClick={addAnswer(qusion.number)} 
+                        type="radio" 
+                        id={answ} 
+                        name={qusion.qusion} />
+                        <label htmlFor={answ}>{answ}</label>
+                    </div>
+                    )}
+                </div>)
+            }
             </div>
         </div>
     );
